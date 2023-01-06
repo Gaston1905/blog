72ff884938561dadb459473f125499bb2ade0d49
Gaston Gaitan <gaston.gaitan04@gmail.com>
2023-01-06 00:42:19 -0300

    page home success, whit responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
